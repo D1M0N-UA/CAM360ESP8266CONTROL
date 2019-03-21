521c6f6980b1a489590abc6a336efbcca0b7bf07
DmitriyMolchanov <rastabox@gmail.com>
2019-03-21 14:06:31 +0200

    add debouncer button

--- CAM360ESP8266CONTROL.ino
+++ CAM360ESP8266CONTROL.ino
@@ -1,7 +1,76 @@
 #include "ESP8266WiFi.h"
+#include <Servo.h>
+#include <Adafruit_NeoPixel.h>
+#include "DRV8825.h"
+#include "Bounce2.h"
+
+
+// pin config
+const int buttonPin = D3;
+Bounce debouncer = Bounce();
+#define servo_tilt D4
+#define DIR D5
+#define STEP D6
+
+// motor config
+#define MOTOR_STEPS 200
+#define RPM 160
+#define MICROSTEPS 1
+Servo tilt;
+DRV8825 stepper(MOTOR_STEPS, DIR, STEP);
+
+// RGB config
+
+const uint16_t PixelCount = 1;
+const uint8_t PixelPin = D1; // pin RGB diode Din
+#define colorSaturation 128
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PixelCount, PixelPin, NEO_GRB + NEO_KHZ800);
+
+int32_t red      = strip.Color (127,0,0);
+int32_t green    = strip.Color (0,127,0);
+int32_t blue     = strip.Color (0,0,127);
+int32_t yellow   = strip.Color (100,127,0);
+int32_t magenta  = strip.Color (100,0,127);
+
+int buttonState = 0;
+int ledState = 0;
 
 WiFiClient client;
 String YI_SSID;
+
+void setup() {
+  // startup process
+  Serial.begin(115200);
+  Serial.println("STARTUP");
+  delay(100);
+
+  pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
+  digitalWrite(BUILTIN_LED, HIGH);   // turn off LED with voltage LOW
+  pinMode(buttonPin, INPUT_PULLUP);  // button
+  debouncer.attach(buttonPin); debouncer.interval(50);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+  // LED red on - init started
+  strip.setPixelColor(0,red);
+  strip.show();
+
+  // stepper settings
+  stepper.begin(RPM, MICROSTEPS);
+  stepper.enable();
+
+  // init servo
+  tilt.attach(servo_tilt);
+  tilt.write(90);
+
+  searchCamera();
+  connectToCamera();
+
+  // LED green on - init started
+    strip.setPixelColor(0,green);
+    strip.show();
+}
 
 void searchCamera() {
   WiFi.mode(WIFI_STA);
@@ -45,17 +114,10 @@ void connectToCamera() {
   Serial.print(" IP con:");
   if (result == true) Serial.print("OK."); else Serial.print("XX.");
   Serial.println();
+
 }
 
-void setup() {
-  //Only For debug
-  Serial.begin(115200);
-  Serial.println("STARTUP");
-  searchCamera();
-  connectToCamera();
-}
-
-// request for new firmware
+// request
 String requestToken() {
 
   client.print("{\"msg_id\":257,\"token\":0}\n\r");
@@ -87,6 +149,19 @@ String requestToken() {
   return token;
 }
 
+void loop() {
+
+		debouncer.update();
+	    if (debouncer.fell()) {
+	    String token = requestToken();
+	    if (token.length() != 0) {
+	      TakePhoto(token);
+	    	}
+
+	    }
+		yield();
+}
+
 void TakePhoto(String token) {
 
   client.print("{\"msg_id\":769,\"token\":");
@@ -101,15 +176,3 @@ void TakePhoto(String token) {
   }
   Serial.println(response);
 }
-
-void loop() {
-
-    String token = requestToken();
-    if (token.length() != 0) {
-      TakePhoto(token);
-    }
-
-	yield();
-
-}
-
